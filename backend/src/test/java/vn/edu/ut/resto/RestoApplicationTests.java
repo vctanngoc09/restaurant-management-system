c925b539ab057f6933098b48e42dac7abd4dfecb
vocaotanngoc <caongoctanvo@gmail.com>
2026-07-31 18:45:57 +0700

    Initialize backend project
    Signed-off-by: vocaotanngoc <caongoctanvo@gmail.com>

---
+++ backend/src/test/java/vn/edu/ut/resto/RestoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ut.resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
